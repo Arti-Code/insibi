b8d9ad0ea4f64adf41e3043e55fe621c253e5833
David T. Weichselbaum <david.t.weichselbaum@gmail.com>
2016-12-05 23:46:41 +0100

    Naive liquid and olfactory sense

--- god.py
+++ god.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/home/david/anaconda3/bin/python
 
 import sys, random
 import pygame
@@ -8,9 +8,18 @@ import pymunk.pygame_util
 from pymunk import Vec2d
 import math
 import numpy as np
+import scipy
+from scipy import ndimage
+# from numba import jit
 
 bX = (-2000, 2000)
 bY = (-2000, 2000)
+wX = bX[1] - bX[0]
+wY = bY[1] - bY[0]
+liquidGrain = 50
+liquidNx = int( (bX[1] - bX[0]) / liquidGrain )
+liquidNy = int( (bY[1] - bY[0]) / liquidGrain )
+dispersion = 1
 spawnSigma = 400
 foodInterval = 5
 mutateInterval = 100
@@ -18,9 +27,13 @@ brown = 5
 pi = 3.1415
 
 maxElasticity = 0.99
-nActivations = 8 # IN: energy OUT: division-prob elasticity # 5 internal states
+nInternal = 5
+states = ["inEnergy", "inSmell", "outDivision", "outElasticity", "outSmell"] + ["internal_%d" % (i+1) for i in range(nInternal)]
+nActivations = len(states)
+states = {name:i for i, name in enumerate(states)}
 mutRange = 0.05
 maxAge = 1000
+smellDegrade = 0.99
 
 massToEn = 100
 maxEn = 3000
@@ -45,7 +58,7 @@ class Particle(object):
 		demand = maxEn - self.energy
 		energy = min(energy, demand) # self can't remove more energy than it can swallow
 		supply = other.energy
-		energy = min(energy, supply)
+		energy = min(energy, supply) # self can't remove more energy than there is in other
 		other.energy -= energy
 		self.energy += energy
 		self.energy -= costTransfer
@@ -115,11 +128,14 @@ class Cell(Particle):
 		self.energy -= rateExist
 		self._adjust_attributes()
 		# regulatory network
-		self.activations[0,0] = self.energy / maxEn
+		self.activations[states["inEnergy"],0] = self.energy / maxEn
+		self.activations[states["inSmell"],0] = get_smell(self.shape.body.position)
 		self.activations = np.tanh( np.dot(self.weights, self.activations) + self.biases ) # drop thrugh tanh non-liniearity like in nn
 		np.clip(self.activations, -1, 1) # clip to range -1..1
-		self.division = self.activations[1,0]
-		self.shape.elasticity = min(maxElasticity, self.activations[2,0] + 1)
+		self.division = self.activations[states["outDivision"],0]
+		self.shape.elasticity = min(maxElasticity, self.activations[states["outElasticity"],0] + 1)
+		outSmell = self.activations[states["outSmell"],0]
+		set_smell(self.shape.body.position, outSmell)
 	def mutate(self):
 		i = random.randint(0, nActivations-1)
 		j = random.randint(0, nActivations-1)
@@ -137,6 +153,7 @@ class Cell(Particle):
 		self.energy -= costDivide
 		self.energy /= 2
 		self._adjust_attributes()
+		self.age = 0
 		x, y = self.shape.body.position
 		newCell = Cell(x, y, self.energy, self)
 		self.mutate()
@@ -153,12 +170,12 @@ class Cell(Particle):
 		radius = radius_from_area(self.energy) # radius
 		radius = max(1, radius)
 		self.shape.unsafe_set_radius(radius)
-
 def radius_from_area(area):
 	if area <= 0: return 0
 	return math.sqrt(area / pi)
 def world_to_view(p):
-	return int((p.x + X) * Z + screenOffX ), int((p.y + Y) * Z + screenOffY)
+	x, y = p
+	return int((x + X) * Z + screenOffX ), int((y + Y) * Z + screenOffY)
 def view_to_world(p):
 	x, y = p
 	return int(((x - screenOffX) / Z ) - X ), int(((y - screenOffY) / Z ) - Y )
@@ -184,6 +201,42 @@ def collision_cell_with_food(arbiter, space, data):
 	cell = a.Particle
 	food = b.Particle
 	cell.steal_energy(food, rateTransfer)
+def disperse_liquid(liquid):
+	return scipy.ndimage.filters.gaussian_filter(liquid, dispersion)
+def set_smell(pos, smell):
+	x, y = pos
+	x = x + (bX[1] - bX[0]) / 2 # x with 0 at leftmost corner of world
+	i = int(x/liquidGrain)
+	y = y + (bY[1] - bY[0]) / 2 # y with 0 at leftmost corner of world
+	j = int(y/liquidGrain)
+	liquid[i,j] = smell
+def get_smell(pos):
+	x, y = pos
+	x = x + (bX[1] - bX[0]) / 2 # x with 0 at leftmost corner of world
+	i = int(x/liquidGrain)
+	y = y + (bY[1] - bY[0]) / 2 # y with 0 at leftmost corner of world
+	j = int(y/liquidGrain)
+	return liquid[i,j]
+def draw_liquid():
+	x1, y1 = view_to_world( (0, 0) ) # get points for rectangle of world coordinates displayed on screen
+	x2, y2 = view_to_world( (screenOffX*2,screenOffY*2) )
+	x1 = x1 + wX/2 # de-center world coordinates, so that top-right of map is 0,0
+	y1 = y1 + wY/2
+	x2 = x2 + wX/2
+	y2 = y2 + wY/2
+	i1 = max(0, min(liquidNx, int(x1/liquidGrain) )) # get indices for liquid
+	j1 = max(0, min(liquidNy, int(y1/liquidGrain) ))
+	i2 = max(0, min(liquidNx, int(x2/liquidGrain) ))
+	j2 = max(0, min(liquidNy, int(y2/liquidGrain) ))
+	recXwidth = int( (screenOffX*2) / (i2-i1) )
+	recYwidth = int( (screenOffY*2) / (j2-j1) )
+	for i in range(i1,i2):
+		for j in range(j1, j2):
+			smell = liquid[i,j]
+			color = (255*min(1, max(0, smell)), 255*min(1, max(0, smell)), 255*min(1, max(0, smell)))
+			x = int( (i-i1) * recXwidth )
+			y = int( (j-j1) * recYwidth )
+			pygame.draw.rect(screen, color, (x,y,recXwidth, recYwidth))
 
 pygame.init()
 infoObject = pygame.display.Info()
@@ -196,15 +249,20 @@ step = 0
 screen = pygame.display.set_mode(dim)
 pygame.display.toggle_fullscreen()
 clock = pygame.time.Clock()
-space = pymunk.Space()
+space = pymunk.Space(threaded=True)
+space.threads = 2 # max number of threads
+space.iterations = 5
 lines = add_borders(space)
+liquid = np.zeros( (liquidNx, liquidNy) )
 particles = []
 colHandler_CF = space.add_collision_handler(1, 2)
 colHandler_CF.post_solve = collision_cell_with_food
 
 running = True
+draw = True
 pause = False
 follow = None
+displaySmell = True
 
 for i in range(50):
 	x = random.gauss(0, spawnSigma)	
@@ -219,20 +277,23 @@ particlesToAdd = []
 
 while running:
 	pygame.display.set_caption("GOD\tFPS: %2.2f\tStep: %8d\t[%d:%d] zoom: %f\t%d" % (clock.get_fps(), step, X, Y, Z, len(particles)))
-	screen.fill((0,0,0))
-	draw_lines(screen, lines)
-	for particle in particles:
-		particle.draw()
-	if follow:
-		X, Y = - follow.shape.body.position
-		font = pygame.font.SysFont("monospace", 36)
-		string = "Energy: %4d  Age: %4d" % (follow.energy, follow.age)
-		text = font.render(string, 1, (255,255,255))
-		textpos = text.get_rect()
-		textpos.centerx = screen.get_rect().centerx
-		screen.blit(text, textpos)
-		if follow.check_remove(): follow = None
-	pygame.display.flip()
+	if draw:
+		screen.fill((0,0,0))
+		draw_lines(screen, lines)
+		if displaySmell:
+			draw_liquid()
+		for particle in particles:
+			particle.draw()
+		if follow:
+			X, Y = - follow.shape.body.position
+			font = pygame.font.SysFont("monospace", 36)
+			string = "Energy: %4d  Age: %4d" % (follow.energy, follow.age)
+			text = font.render(string, 1, (255,255,255))
+			textpos = text.get_rect()
+			textpos.centerx = screen.get_rect().centerx
+			screen.blit(text, textpos)
+			if follow.check_remove(): follow = None
+		pygame.display.flip()
 
 	if not pause:
 		if step % foodInterval == 0:
@@ -252,6 +313,10 @@ while running:
 		particles.extend(particlesToAdd)
 		particlesToRemove = []
 		particlesToAdd = []
+
+		liquid = disperse_liquid(liquid)
+		liquid *= smellDegrade
+		np.clip(liquid, 0, 1)
 		space.step(1/50.0)
 		clock.tick(50)
 		step += 1
@@ -284,6 +349,10 @@ while running:
 			pygame.display.toggle_fullscreen()
 		if event.type == KEYDOWN and event.key == K_SPACE:
 			pause = not pause
+		if event.type == KEYDOWN and event.key == K_d:
+			draw = not draw
+		if event.type == KEYDOWN and event.key == K_s:
+			displaySmell = not displaySmell
 		if event.type == pygame.MOUSEBUTTONUP and event.button == 1: # left=1
 			mousePos = pygame.mouse.get_pos()
 			mousePos = view_to_world(mousePos)
